d66868c087c837ff77ace251ece586ec0cb3584d
Josias Besodia <djodev23@gmail.com>
2025-06-09 12:49:45 +0200

    navbar set

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,8 +2,8 @@ import { Fragment } from "react"
 
 function NavBar() {
     return <Fragment>
-        <nav className="bg-black h-20 w-full opacity-90">
-            <p>Lorem ipsum dolor sit amet.</p>
+        <nav className="bg-black h-20 w-full opacity-90 relative items-center flex px-10">
+            <img className="h-16 w-auto" src="/src/assets/logo&name.png" alt="logo de fertilia" />
         </nav>
     </Fragment>
 }
