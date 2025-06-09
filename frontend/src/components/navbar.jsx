e4063d1d4dca1473c4638ae10baea8df1ebf147f
Josias Besodia <djodev23@gmail.com>
2025-06-09 13:02:28 +0200

    change2

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import { Fragment } from "react"
 
 function NavBar() {
     return <Fragment>
-        <nav className="bg-black h-20 w-full opacity-90 relative items-center flex px-10 text-white">
+        <nav className="bg-black h-20 w-full opacity-90 relative items-center flex px-10 text-white justify-between">
             <ul className="">
                 <img className="h-16 w-auto hidden md:flex" src="/src/assets/logo&name.png" alt="logo de fertilia" />
                 <img className="h-16 w-auto md:hidden" src="/src/assets/logo_only.png" alt="logo de fertilia" />
