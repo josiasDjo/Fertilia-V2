03c49ba8b1ebfad32393fa056734d02e92e00e5c
Josias Besodia <djodev23@gmail.com>
2025-06-09 12:51:02 +0200

    change1

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,7 +3,10 @@ import { Fragment } from "react"
 function NavBar() {
     return <Fragment>
         <nav className="bg-black h-20 w-full opacity-90 relative items-center flex px-10">
-            <img className="h-16 w-auto" src="/src/assets/logo&name.png" alt="logo de fertilia" />
+            <ul>
+                <img className="h-16 w-auto" src="/src/assets/logo&name.png" alt="logo de fertilia" />
+                <img className="h-16 w-auto" src="/src/assets/logo&name.png" alt="logo de fertilia" />
+            </ul>
         </nav>
     </Fragment>
 }
