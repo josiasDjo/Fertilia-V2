0daff121f05d22dc972b504c6a7726e09c85a901
Josias Besodia <djodev23@gmail.com>
2025-06-09 12:46:16 +0200

    svg set

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,8 +2,8 @@ import { Fragment } from "react"
 
 function NavBar() {
     return <Fragment>
-        <nav>
-            
+        <nav className="bg-black h-20 w-full opacity-90">
+            <p>Lorem ipsum dolor sit amet.</p>
         </nav>
     </Fragment>
 }
