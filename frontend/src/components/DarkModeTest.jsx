068b6af60f126f9b0c7c0b8521e08dfe28b4e412
josiasDjo <djodev23@gmail.com>
2025-07-15 19:26:56 +0200

    DarkMode set change2

--- frontend/src/components/DarkModeTest.jsx
+++ frontend/src/components/DarkModeTest.jsx
@@ -17,11 +17,11 @@ export default function DarkTest() {
   }, [darkMode]);
 
   return (
-    <div className="bg-white dark:bg-gray-700 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+    <div id="testDark" className="bg-white text-slate-500 dark:bg-slate-500 dark:text-white rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
       <h1 className="text-3xl mb-6">Dark Mode Test</h1>
       <button
         onClick={() => setDarkMode((prev) => !prev)}
-        className="px-6 py-3 rounded bg-gray-300 dark:bg-gray-800"
+        className="px-6 py-3 rounded bg-gray-300 text-gray-800 dark:bg-gray-800 dark:text-white"
       >
         Toggle Theme ({darkMode ? 'Sombre' : 'Clair'})
       </button>
