22a572e2b36e422d78483e149e1c9fc16d6be3e3
josiasDjo <djodev23@gmail.com>
2025-07-21 11:53:58 +0200

    Dashoboard change10

--- frontend/src/pages/dashboards/dashbord.jsx
+++ frontend/src/pages/dashboards/dashbord.jsx
@@ -1,12 +1,13 @@
 import NavBarDash from "./navbarDash";
 import SideBar from "./sidebar";
-
+import MainPage from "./main";
 
 export default function Dashboard() {
     return <>
         <div class="w-full h-screen relative flex flex-row bg-skin-bg">
             <NavBarDash />
             <SideBar />
+            <MainPage />
         </div>
     </>
 }
---
+++ frontend/src/pages/dashboards/main.jsx
@@ -0,0 +1,9 @@
+
+
+export default function MainPage() {
+    return (
+        <main className="w-full lg:w-[calc(100%-16rem)] lg:h-[calc(100%-2rem)] bg-gray-700">
+        
+        </main>
+    )
+}
