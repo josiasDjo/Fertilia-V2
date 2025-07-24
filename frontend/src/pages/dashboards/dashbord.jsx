8ac7560e0c440a17a618ab8fbf7a93c1d49aa92c
josiasDjo <djodev23@gmail.com>
2025-07-24 20:22:46 +0200

    react-toastify configure

--- frontend/src/pages/dashboards/dashbord.jsx
+++ frontend/src/pages/dashboards/dashbord.jsx
@@ -1,10 +1,14 @@
 import NavBarDash from "./navbarDash";
 import SideBar from "./sidebar";
 import MainPage from "./main";
+// import { ToastContainer, toast } from "react-toastify";
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function Dashboard() {
     return <>
         <div class="w-full h-screen relative flex flex-row bg-gray-200 dark:bg-skin-bg">
+            {/* {toast.success("Connexion réussie !")} */}
+            {/* {alert("Connexion réussie")} */}
             <NavBarDash />
             <SideBar />
             <MainPage />
