e93f24ee829c7406595284ccbf8a4b0160b30263
josiasDjo <djodev23@gmail.com>
2025-07-27 19:37:42 +0200

    google analytic set change2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 import './i18n';
 import NavBar from './components/navbar';
 import Home from './pages/home';
-import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation  } from 'react-router-dom';
 import  Signin from './pages/signin';
 import Signup from './pages/signup';
 import { ResetPass } from './pages/resetPassword';
@@ -12,9 +12,10 @@ import { IrrigationAutomatiquePage } from './pages/irrigation';
 import { AnalyseRendementsPage } from './pages/analyseRendement';
 import Faq from './pages/faq';
 import Dashboard from './pages/dashboards/dashbord';
+import { initGA, trackPageview } from './hooks/analytics';
 
 export default function App() {
-
+  const location = useLocation()
   const userTheme = localStorage.getItem('theme'); // 'light' | 'dark' | null
   const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
@@ -23,6 +24,14 @@ export default function App() {
   } else {
     document.documentElement.classList.remove('dark');
   }
+
+  useEffect(() => {
+    initGA()
+  }, [])
+
+  useEffect(() => {
+    trackPageview(location.pathname + location.search)
+  }, [location])
 
   return (
     <Router>
