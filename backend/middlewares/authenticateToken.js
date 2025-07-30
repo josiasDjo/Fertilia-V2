68b7626052e580d181b56a4ff8e146ca7194e10c
josiasDjo <djodev23@gmail.com>
2025-07-30 11:52:44 +0200

    Get fields for a specific user change4

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -5,15 +5,12 @@ const authenticateToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-        req.flash('error_msg', 'Token manquant');
         return res.json({success: false, message: 'Token manquant', redirectTo: "/signin"});
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            req.flash('error_msg', 'Token invalide ou expiré');
             return res.json({success: false, message: "Token invalide ou expiré", redirectTo: "/signin"});
-
         }
         req.user = user;
         next();
